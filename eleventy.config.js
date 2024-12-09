1dc733fd0034c3c2ee0974cc7c4795cec12b08d9
Jérôme Coupé <jercoupe@gmail.com>
2024-12-09 02:27:23 +0100

    add favicon, robots.txt

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,6 +19,7 @@ export default async function (eleventyConfig) {
   // passthrough copy
   eleventyConfig.addPassthroughCopy("./src/assets/fonts/");
   eleventyConfig.addPassthroughCopy("./src/assets/img/");
+  eleventyConfig.addPassthroughCopy({ "./src/static": "/" });
 
   // server options
   eleventyConfig.setServerOptions({
