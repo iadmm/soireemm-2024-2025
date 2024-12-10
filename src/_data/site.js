ed52621846b36aec8e26916258ca1022865e5df5
Jérôme Coupé <jercoupe@gmail.com>
2024-12-10 14:53:05 +0100

    temporary netlify domain

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ export default {
   eventDate: new Date("2025-01-23"),
   metaTitle: "Beers and Careers",
   metaDescription: "Soirée métiers Multimédia 2024-2025.",
-  url: "https://metiers.iadmm.be/",
+  url: "https://soireemm2025.netlify.app",
   github: "https://github.com/jeromecoupe/soireemm-2024-2025",
   registrations: {
     active: true,
