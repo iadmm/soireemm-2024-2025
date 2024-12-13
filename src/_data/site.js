5b636cd6ed362a9b04e68e0fd0e443b104d265b0
Jérôme Coupé <jercoupe@gmail.com>
2024-12-13 21:00:37 +0100

    add github repository

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ export default {
   metaTitle: "Beers and Careers",
   metaDescription: "Soirée métiers Multimédia 2024-2025.",
   url: "https://soireemm2025.netlify.app",
-  github: "https://github.com/jeromecoupe/soireemm-2024-2025",
+  github: "https://github.com/iadmm/soireemm-2024-2025",
   registrations: {
     active: true,
     url: "https://forms.gle/qTDecizLnZKGvykV9",
