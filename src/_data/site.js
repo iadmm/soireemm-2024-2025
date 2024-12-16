05c193efd07d9d9a6189732634498f311d01a85d
Jérôme Coupé <jercoupe@gmail.com>
2024-12-16 07:01:41 +0100

    fix: production URL

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ export default {
   eventDate: new Date("2025-01-23"),
   metaTitle: "Bières et Carrières",
   metaDescription: "Soirée métiers Multimédia 2024-2025.",
-  url: "https://soireemm2025.netlify.app",
+  url: "https://metiers.iadmm.be",
   github: "https://github.com/iadmm/soireemm-2024-2025",
   registrations: {
     active: true,
